36543803e9ee72981e66e1f013af06e903858c02
Dhruv <dp397113@gmail.com>
2024-09-28 22:17:46 +0530

    Update __init__.py

--- src/tts/__init__.py
+++ src/tts/__init__.py
@@ -1,2 +1,2 @@
-from tts.engine import generate_audio, play_audio_sequence
-from tts.audio import generate_audio_files_multiprocessing
+from tts.engine import generate_audio
+from tts.audio import start_audio_processes
